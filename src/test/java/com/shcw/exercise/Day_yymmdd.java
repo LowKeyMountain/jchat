99c7b6657aedd2aac6b27f16e1135387b41384e8
LowKeyMountain <zoushenghang_80@163.com>
2020-04-19 20:28:06 +0800

    每天学习练习类。

---
+++ src/test/java/com/shcw/exercise/Day_yymmdd.java
@@ -0,0 +1,15 @@
+package com.shcw.exercise;
+
+/**
+ * 
+ *  学习xxx知识练习
+ * 
+ * @author Michael 2020年4月19日 下午8:25:27
+ */
+public class Day_yymmdd {
+	
+	public static void main(String [] args) {
+		System.out.println("Hello Java!");
+	}
+	
+}
